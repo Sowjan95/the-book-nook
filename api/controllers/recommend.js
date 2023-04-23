d183017a796836729d9883fab1520f14bb60d112
Sowjan95 <sowjanya.sritharasarma@macaulay.cuny.edu>
2023-04-23 18:56:28 -0400

    added delete rec and get all user's recs

--- api/controllers/recommend.js
+++ api/controllers/recommend.js
@@ -5,32 +5,16 @@ const passport = require("../middlewares/authentication");
 const { User, Recommendation, Book } = db;
 
 
-// get all recommendations
+// get all recommendations for the user
 router.get("/", passport.isAuthenticated(), async (req, res) => {
     const user = req.user;
     const recs = await Recommendation.findAll({
         where: {
-          id: user.id
-        },
-        include: "Recommendation"
+          UserId: user.id
+        }
       });
     res.json(recs);
 });
-
-
-//get friend by username
-// router.get("/username/:username", passport.isAuthenticated(), async (req, res) => {
-//     const user = req.user
-
-//     const {username}  = req.params;
-//     let friendUser = await User.findOne({
-//         where:{
-//             id: user.id,
-//         },
-//         include: "Friend"
-//     });
-//     res.json(friendUser);
-// });
 
 
 // post a new recommendation
@@ -56,26 +40,20 @@ router.post("/", passport.isAuthenticated(), async (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
-    // user.addRecommendation(friend, book).then((newRec) => {
-    //     res.status(201).json(newRec);
-    //   })
-    //   .catch((err) => {
-    //     res.status(400).json(err);
-    //   });
   });
   
 
-// delete a friend
-// router.delete("/:id", (req, res) => {
-//   const { id } = req.params;
-//   Friend.findByPk(id).then((friend) => {
-//     if (!friend) {
-//       return res.sendStatus(404);
-//     }
-//     friend.destroy();
-//     res.sendStatus(204);
-//   });
-// });
+// delete a recommendation
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Recommendation.findByPk(id).then((recommendation) => {
+    if (!recommendation) {
+      return res.sendStatus(404);
+    }
+    recommendation.destroy();
+    res.sendStatus(204);
+  });
+});
 
 
 module.exports = router;
